977bd62b6d7e937accf728831cbc1ffcae70f006
RushiRaj-001 <rushirajapure111@gmail.com>
2024-04-11 11:18:28 +0530

    Day 3

---
+++ Day 3/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+    public static void findTriplet(int[] arr){
+
+        for(int i = 0; i<arr.length;i++){
+            for(int j = i+1; j<arr.length;j++){
+                for(int k = j+1;k<arr.length;k++){
+                    if(arr[i]+arr[j]+arr[k] == 0){
+                        System.out.println("["+arr[i]+" "+arr[j]+" "+arr[k]+"]");
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { -1, 0, 1, 2, -1, -4 };
+        findTriplet(nums);
+    }
+}
